76ac4b232a6e26110c81cc43e7fe66dd6397226f
Sergey <59033208+Immsan@users.noreply.github.com>
2020-01-24 23:03:16 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+      pagination: {
+         el: '.project-pagination',
+         bulletClass: 'project-bulet',
+         bulletActiveClass: 'project-bulet-active',
+         clickable: true
+      },
+   });
